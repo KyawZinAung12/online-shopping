58687d97e34dfecddf77696f54201a446400966a
heinminhtet8138 <heinminhtet8138@gmail.com>
2020-10-12 16:53:52 +0630

    item_detail_delete

--- backend/item_list.php
+++ backend/item_list.php
@@ -53,8 +53,24 @@ include 'dbconnect.php';
 							<td>1</td>
 							<td><?php echo $item['name']; ?></td>
 							<td><?php echo $item['codeno']; ?></td>
-							<td><?php echo $item['price']; ?></td>
-							<td><a href="#" class="btn btn-outline-primary btn-sm">Detail</a> <a href="#" class="btn btn-outline-warning btn-sm">Edit</a> <a href="#" class="btn btn-outline-danger btn-sm">Delete</a></td>
+							<td>
+								<?php 
+
+									if ($item['discount']) {
+										echo $item['discount']." MMK";
+								?>
+
+								<del><?php echo $item['price']." MMK"; ?></del>
+								<?php 
+
+									}else{
+										echo $item['price']." MMK";
+									}
+
+								?>
+									
+								</td>
+							<td><a href="item_detail.php?id=<?php echo $item['id'] ?>" class="btn btn-outline-primary btn-sm">Detail</a> <a href="#" class="btn btn-outline-warning btn-sm">Edit</a> <a href="item_delete.php?id=<?php echo $item['id'] ?>" class="btn btn-outline-danger btn-sm">Delete</a></td>
 
 						</tr>
 
